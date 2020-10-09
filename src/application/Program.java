5081b5499a1b45fa0a37da28fad787e1f214fd5e
Sergio Pelegrini <serpel@gmail.com>
2020-10-09 09:14:26 -0300

    filereader e bufferedreader

---
+++ src/application/Program.java
@@ -0,0 +1,49 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		String path = "c:\\temp\\in.txt";
+		/*
+		FileReader fr = null;
+		BufferedReader br = null;
+		*/
+		//try {
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			/*
+			fr = new FileReader(path);
+			br = new BufferedReader(fr);
+			*/
+			String line = br.readLine();
+			
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+		}
+		catch (IOException e) {
+			System.out.println("Erro: " + e.getMessage());
+		}
+		/*
+		finally {
+			try {
+				if (br != null) {
+					br.close();
+				}
+				if (fr != null) {
+					fr.close();
+				}
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		*/
+	}
+
+}
